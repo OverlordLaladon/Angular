b304bf66cd61336e89d9967012d41e72c795ca9b
OverlordLaladon <lalagame@fidy.hu>
2018-05-20 12:47:30 +0200

    Promise Part 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -30,9 +30,9 @@ export class HomeComponent implements OnInit {
     this.dishservice.getFeaturedDish().then(dish => (this.dish = dish));
     this.promotionservice
       .getFeaturedPromotion()
-      .then(promotion => (this.promotion = this.promotion));
+      .then(promotion => (this.promotion = promotion));
     this.leaderservice
       .getFeaturedLeader()
-      .then(leader => (this.leader = this.leader));
+      .then(leader => (this.leader = leader));
   }
 }
